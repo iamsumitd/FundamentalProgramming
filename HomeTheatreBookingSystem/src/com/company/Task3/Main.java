588d1fb8f706ae1c6a07ee9d7527ca232bf1cb9a
Sumit Dubey <nordicandersson@gmail.com>
2019-11-01 09:41:07 +0100

    Task 3, added after completion

--- HomeTheatreBookingSystem/src/com/company/Task3/Main.java
+++ HomeTheatreBookingSystem/src/com/company/Task3/Main.java
@@ -1,4 +1,31 @@
 package com.company.Task3;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main (String[] args) {
+
+        Main myApp = new Main();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("----Christmas presents label program----");
+        System.out.print("Enter the sender's initials: ");
+        String sender = input.nextLine();
+        System.out.print("Enter the recipient's initials: ");
+        String recipient = input.nextLine();
+
+        String a;
+        a = myApp.presentLabel(sender, recipient);
+        System.out.println(a);
+    }
+
+    private String presentLabel (String sender, String recipient){
+        return "Here's your label"+"\n"+
+                "**************"+"\n"+
+                "* From: "+sender+" *"+"\n"+
+                "* To  : "+recipient+" *"+"\n"+
+                "**************";
+    }
+
 }
--- HomeTheatreBookingSystem/src/com/company/Task4/Main.java
+++ HomeTheatreBookingSystem/src/com/company/Task4/Main.java
@@ -1,4 +1,11 @@
 package com.company.Task4;
 
 public class Main {
+
+    public static void main (String[] args){
+
+        String[] queue = new String [100];
+
+        System.out.println("----Registration Menu----");
+    }
 }
