aec1cb7cb311b9aa2ba668fbf4f789110c280ad7
Sumit Dubey <nordicandersson@gmail.com>
2019-11-01 11:56:57 +0100

    Task 4 under development...

--- HomeTheatreBookingSystem/src/com/company/Task4/Main.java
+++ HomeTheatreBookingSystem/src/com/company/Task4/Main.java
@@ -1,11 +1,51 @@
 package com.company.Task4;
 
+
+//under development...
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        String[] queue = new String [100];
+        Scanner input = new Scanner(System.in);
+        String[] queue = new String[100];
 
-        System.out.println("----Registration Menu----");
+        while (true) {
+            //Menu
+            System.out.println("---- Food Menu ----");
+            System.out.println("1. Register a new visitor");//placing the visitors in the queue
+            System.out.println("2. Advance the queue");//remove the visitor from the queue
+            System.out.println();
+            System.out.println("Enter option: ");
+            int option = input.nextInt();
+
+            int counter=0; //why can't I define the variable here and use it in the for loop
+            if (option == 1) {
+
+                for (counter < queue.length; counter++) {
+                    System.out.println("Enter your name: ");
+
+                  while (queue[counter]!=null)
+                  {counter++;}
+
+                  queue[counter] = input.nextLine();
+
+                }
+            }else if(option == 2){
+
+                for (int newCounter = 0; newCounter<queue.length; newCounter--){
+                    queue[counter-1] =
+                }
+
+
+                System.out.println("Now serving: "+queue[0]);
+                queue[0] = null;
+
+
+
+            }
+
+        }
     }
 }
